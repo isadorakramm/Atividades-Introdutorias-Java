bb082648f583c88a11b071f36befebc8f8a88935
Isa Kramm <134699029+isadorakramm@users.noreply.github.com>
2025-08-11 09:57:46 -0300

    Update Cadastro.java

--- Cadastro.java
+++ Cadastro.java
@@ -1,14 +1,16 @@
 
 import java.util.Scanner;
 
-class Cadastra {
+class Cadastro {
     public static void main (String[] args) {
         Scanner ler = new Scanner(System.in);
         int idade;
         String nome;
 
-        System.out.printf("Bem vindo ao menu ");
+        System.out.printf("Bem vindo ao menu, qual é o seu nome? ");
         nome = ler.nextLine();
+        System.out.printf("Quantos anos você tem? ");
+        idade = ler.nextInt();
 
         System.out.printf("Seu nome é %s e você tem %d anos.", nome, idade);
         System.exit(0);
